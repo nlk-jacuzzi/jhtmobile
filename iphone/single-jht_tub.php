f8fc52aedfce1b12f43194b6777a13e73b8f85a0
Tim Spinks <tim@ninthlink.com>
2015-08-06 12:34:44 -0700

    copy button updates

--- iphone/single-jht_tub.php
+++ iphone/single-jht_tub.php
@@ -91,8 +91,8 @@ dataLayer.push({
             <div class="msrp-container" style="display:none;">
                 <?php echo '<p class="msrp-price"><span>' . $msrp . '</span> MSRP</p>'; ?>
                 <p class="msrp-disclaimer"><strong>Disclaimer: </strong>Prices listed are Manufacturer's Suggested Retail Price (MSRP). Prices may not include additional fees, see authorized dealer for details.</p>
-                <a class="msrp-dealer" href="<?php bloginfo('url'); ?>/dealer-locator/">Find Your Nearest Dealer</a>
-                <a class="msrp-pricing" href="<?php bloginfo('url'); ?>/get-a-quote/?tid=<?php echo $post->ID; ?>">Request Pricing from Dealer</a>
+                <a class="msrp-dealer" href="<?php bloginfo('url'); ?>/dealer-locator/">Find A Dealer</a>
+                <a class="msrp-pricing" href="<?php bloginfo('url'); ?>/get-a-quote/?tid=<?php echo $post->ID; ?>">Get Pricing</a>
             </div>
         <?php endif; ?>
         <div class="moc">
