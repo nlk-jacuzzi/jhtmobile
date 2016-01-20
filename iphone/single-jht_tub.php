d855e318a0e71ee12c565a8cf6e879475af18cc8
Tim Spinks <tim@ninthlink.com>
2016-01-20 09:03:03 -0800

    show reviews block

--- iphone/single-jht_tub.php
+++ iphone/single-jht_tub.php
@@ -169,9 +169,9 @@ dataLayer.push({
                 <a href="<?php echo get_bloginfo('url'); ?>/color-selector/">View Color Selector</a>
             </h3>
         </div>
-        <?php /*<ul class="tubinfo">
+        <ul class="tubinfo">
             <li><div id="BVRRContainer"><?php echo $bv->reviews->getContent();?></div></li>
-        </ul> */ ?>
+        </ul>
 
 <?php
 	}
